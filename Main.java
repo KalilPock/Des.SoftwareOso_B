d335b9bceb5f1aec354e25666984c18a0f7177c0
kalilpock <kalilpock54@gmail.com>
2025-03-27 20:26:11 -0300

    criacaoClasseCalculadora

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Bem-Vindo à calculadora");
+
+        System.out.println("Digite o primeiro número: ");
+        int entrada1;
+        Scanner scanner = new Scanner(System.in);
+        entrada1 = scanner.nextInt();
+        System.out.println("O primeiro número é: " + entrada1);
+
+        //Captura o 2 numero
+        System.out.println("Digite o segundo número: ");
+        int entrada2;
+        entrada2 = scanner.nextInt();
+        System.out.println("O segundo número é: " + entrada2);
+
+        //seleciona a operação desejada
+        // System.out.println("Deseja: ");
+        // String operacao;
+        // operacao = scanner.nextInt();
+
+
+
+
     }
 }
