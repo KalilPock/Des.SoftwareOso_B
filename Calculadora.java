7c3467ee4c62d73b1a2c1c3ce08884ce45e94bb0
kalilpock <kalilpock54@gmail.com>
2025-03-27 21:01:17 -0300

    atividadeFinalizada

--- Calculadora.java
+++ Calculadora.java
@@ -1,23 +1,23 @@
-public class Calculadora{
+
+public class Calculadora {
     public static void main(String[] args) {
-       
-        int resultado = somar(5, 3);
-        System.out.println("O resultado da soma é: " + resultado);
-        }
 
-        public static int somar(int a, int b) {
+    }
+
+    public static int somar(int a, int b) {
         return a + b;
-        }
+    }
 
-        public static int subtrair(int a, int b) {
-            return a - b;
-        }
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
 
-        public static int  multiplicar(int a, int b){
-            return a * b;
-        }
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
 
-        public static int dividir(int a, int b){
-            return a / b;
-        }
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -2,24 +2,59 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bem-Vindo à calculadora");
+        boolean continuar = true;
 
-        System.out.println("Digite o primeiro número: ");
-        int entrada1;
-        Scanner scanner = new Scanner(System.in);
-        entrada1 = scanner.nextInt();
-        System.out.println("O primeiro número é: " + entrada1);
+        do { 
+            System.out.println("Bem-Vindo à calculadora");
 
-        //Captura o 2 numero
-        System.out.println("Digite o segundo número: ");
-        int entrada2;
-        entrada2 = scanner.nextInt();
-        System.out.println("O segundo número é: " + entrada2);
+            Scanner scanner = new Scanner(System.in);
+    
+            System.out.println("Digite o primeiro número:");
+            int entrada1 = scanner.nextInt();
+    
+            System.out.println("Digite o segundo número:");
+            int entrada2 = scanner.nextInt();
+    
+            System.out.println("Escolha a operação: 1-Somar, 2-Subtrair, 3-Multiplicar, 4-Dividir");
+            int operacao = scanner.nextInt();
+    
+            switch (operacao) {
+                case 1:
+                    System.out.println("**************************************");
+                    System.out.println("Resultado: " + Calculadora.somar(entrada1, entrada2));
+                    break;
+                case 2:
+                     System.out.println("**************************************");
+                    System.out.println("Resultado: " + Calculadora.subtrair(entrada1, entrada2));
+                    break;
+                case 3:
+                    System.out.println("**************************************");
+                    System.out.println("Resultado: " + Calculadora.multiplicar(entrada1, entrada2));
+                    break;
+                case 4:
+                    if (entrada2 != 0) {
+                                    System.out.println("**************************************");
+                                    System.out.println("Deseja continuar? 1-Sim, 2-Não");
+                        System.out.println("Resultado: " + Calculadora.dividir(entrada1, entrada2));
+                    } else {
+                        System.out.println("**************************************");
+                        System.out.println("Erro: Divisão por zero não é permitida.");
+                    }
+                    break;
+                default:
+                    System.out.println("Operação inválida.");
+                    scanner.close();
+            }
 
-        //seleciona a operação desejada
-        // System.out.println("Deseja: ");
-        // String operacao;
-        // operacao = scanner.nextInt();
+            System.out.println("**************************************");
+            System.out.println("Deseja continuar? 1-Sim, 2-Não");
+            int continuarOp = scanner.nextInt();
+            if (continuarOp == 2) {
+                continuar = false;
+            }
+
+        } while (continuar = true);
+
 
 
 
