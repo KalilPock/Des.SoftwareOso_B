d422ef2fd45560eabea58ec9f312b4805834fcc9
kalilpock <kalilpock54@gmail.com>
2025-03-31 20:25:07 -0300

    testes

--- exercicio2/Main.java
+++ exercicio2/Main.java
@@ -4,6 +4,7 @@ public class Main{
     public static void main(String[] args) {
 
         boolean sair = true;      
+        
         do {
 
             System.out.println("Bem-Vindo ao acervo de livros!");
