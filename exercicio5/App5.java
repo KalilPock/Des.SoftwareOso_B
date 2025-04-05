04e51b774da26a78459421cefbd7c9cec16277d8
kalilpock <kalilpock54@gmail.com>
2025-04-05 16:09:30 -0300

    iniciacaoAtividade5

---
+++ exercicio5/App5.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class App5{
+
+    public static void main(String[] args) {
+        System.out.println("Bem-Vindo ao sistema de jogadores!");
+        ArrayList<Carros> carros = new ArrayList<>();
+        Carros.exibirMenu(carros); // Lógica do menu feita na classe Jogador
+    }
+    
+
+}
---
+++ exercicio5/Carro.java
@@ -0,0 +1,14 @@
+
+public class Carro {
+
+    private int ano;
+    private String marca;
+    private String modelo;
+    private double velocidadeAtual;
+    private String placa;
+
+    public static void exibirMenu(String[] args) {
+        System.out.println("Bem-Vindo ao sistema de carros!");
+    }
+    
+}
