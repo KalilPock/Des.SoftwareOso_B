0f3d234a524aee6ae0d9d94419c3c704686434be
kalilpock <kalilpock54@gmail.com>
2025-04-05 15:26:44 -0300

    refatoraçãoOrganizacaoMenusEMetodos

--- exercicio4/App4.java
+++ exercicio4/App4.java
@@ -1,90 +1,10 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class App4 {
 
     public static void main(String[] args) {
         System.out.println("Bem-Vindo ao sistema de jogadores!");
-        Scanner scanner = new Scanner(System.in);
         ArrayList<Jogador> jogadores = new ArrayList<>();
-        boolean sair = true;
-        int opcao; 
-
-        do { 
-            System.out.println("Escolha uma opção:");
-            System.out.println("1 - Criar Jogador");
-            System.out.println("2 - Aumentar Pontuação");
-            System.out.println("3 - Diminuir Pontuação");
-            System.out.println("4 - Zerar Pontuação");
-            System.out.println("5 - Aumentar Nível");
-            System.out.println("6 - Consultar Jogador");
-            System.out.println("7 - Sair");
-            opcao = scanner.nextInt(); // Atualiza a opção escolhida pelo usuário
-
-            switch (opcao) {
-                case 1 :
-                    System.out.println("Digite o nome do jogador:");
-                    String nome = scanner.next();
-                    System.out.println("Digite a pontuação do jogador:");
-                    int pontuacao = scanner.nextInt();
-                    System.out.println("Digite o nível do jogador:");
-                    int nivel = scanner.nextInt();
-                    Jogador jogador = new Jogador(nome, pontuacao, nivel);
-                    jogadores.add(jogador);
-                    System.out.println("Jogador criado com sucesso!");
-                    break;
-                case 2 :
-                System.out.println("Digite o nome do jogador:");
-                String nomeAumento = scanner.next();
-                System.out.println("Digite a quantidade de pontos a aumentar:");
-                int pontosAumento = scanner.nextInt();
-                boolean jogadorEncontrado = false;
-
-                for (Jogador j : jogadores) {
-                    if (j.getNome().equalsIgnoreCase(nomeAumento)) {
-                        j.aumentarPontuacao(pontosAumento);
-                        System.out.println("Pontuação aumentada com sucesso!");
-                        jogadorEncontrado = true;
-                        break;
-                    }
-                }
-                if (!jogadorEncontrado) {
-                    System.out.println("Jogador não encontrado.");
-                }
-                break;
-                case 3 :
-                    // Implementar lógica para aumentar nível
-                    break;
-                case 4 :
-                    // Implementar lógica para diminuir pontuação
-                    break;
-                case 5 :
-                    // Implementar lógica para zerar pontuação
-                    break;
-                case 6 :
-                    System.out.println("Digite o nome do jogador que deseja consultar:");
-                    String nomeConsulta = scanner.next();
-                    boolean encontrado = false;
-                    for (Jogador j : jogadores) {
-                        if (j.getNome().equalsIgnoreCase(nomeConsulta)) {
-                            j.exibirInfo();
-                            encontrado = true;
-                            break;
-                        }
-                    }
-                    if (!encontrado) {
-                        System.out.println("Jogador não encontrado.");
-                    }
-
-                    break;
-                case 7 :
-                    sair = false;
-                    break;
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
-            }
-        } while (sair);
-        System.out.println("Sistema encerrado.");
-        scanner.close();
+        Jogador.exibirMenu(jogadores); // Delegando a lógica do menu para a classe Jogador
     }
 }
--- exercicio4/Jogador.java
+++ exercicio4/Jogador.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Jogador {
-    
-    private String nome; 
+
+    private String nome;
     private int pontuacao;
     private int nivel;
 
@@ -9,45 +12,144 @@ public class Jogador {
         this.pontuacao = pontuacao;
         this.nivel = nivel;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public int getPontuacao() {
         return pontuacao;
     }
+
     public void setPontuacao(int pontuacao) {
         this.pontuacao = pontuacao;
     }
+
     public int getNivel() {
         return nivel;
     }
+
     public void setNivel(int nivel) {
         this.nivel = nivel;
     }
 
-
-    public void aumentarPontuacao(int pontos) {
+    public void atualizarPontuacao(int pontos) {
         this.pontuacao += pontos;
     }
-    public void aumentarNivel(int nivel) {
-        this.nivel += nivel;
-    }
-    public void diminuirPontuacao(int pontos) {
-        this.pontuacao -= pontos;
-    }
-    public void zerarPontuacao() {
-        this.pontuacao = 0;
+
+    public void exibirInfo() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Pontuação: " + pontuacao);
+        System.out.println("Nível: " + nivel);
     }
 
-    public void exibirInfo(){
-        System.out.println("Exibindo as Informações do jogador! :");
-        System.out.println("Nome:" + nome);
-        System.out.println("Nivel:" + nivel);
-        System.out.println("Pontuação:" + pontuacao);
+    public static void exibirMenu(ArrayList<Jogador> jogadores) {
+        Scanner scanner = new Scanner(System.in);
+        boolean sair = true;
+
+        do {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Criar Jogador");
+            System.out.println("2 - Atualizar Pontuação");
+            System.out.println("3 - Atualizar Nível");
+            System.out.println("4 - Consultar Jogador");
+            System.out.println("5 - Sair");
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    criarJogador(jogadores, scanner);
+                    break;
+                case 2:
+                    atualizarPontuacao(jogadores, scanner);
+                    break;
+                case 3:
+                    atualizarNivel(jogadores, scanner);
+                    break;
+                case 4:
+                    consultarJogador(jogadores, scanner);
+                    break;
+                case 5:
+                    sair = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while (sair);
+
+        System.out.println("Sistema encerrado.");
+        scanner.close();
     }
 
+    private static void criarJogador(ArrayList<Jogador> jogadores, Scanner scanner) {
+        System.out.println("Digite o nome do jogador:");
+        String nome = scanner.next();
+        System.out.println("Digite a pontuação do jogador:");
+        int pontuacao = scanner.nextInt();
+        System.out.println("Digite o nível do jogador:");
+        int nivel = scanner.nextInt();
+        Jogador jogador = new Jogador(nome, pontuacao, nivel);
+        jogadores.add(jogador);
+        System.out.println("Jogador criado com sucesso!");
+    }
+
+    private static void atualizarPontuacao(ArrayList<Jogador> jogadores, Scanner scanner) {
+        System.out.println("Digite o nome do jogador:");
+        String nomeAumento = scanner.next();
+        System.out.println("Digite a quantidade de pontos a aumentar:");
+        int pontosAumento = scanner.nextInt();
+        boolean jogadorEncontrado = false;
+
+        for (Jogador j : jogadores) {
+            if (j.getNome().equalsIgnoreCase(nomeAumento)) {
+                j.atualizarPontuacao(pontosAumento);
+                System.out.println("Pontuação aumentada com sucesso!");
+                jogadorEncontrado = true;
+                break;
+            }
+        }
+        if (!jogadorEncontrado) {
+            System.out.println("Jogador não encontrado.");
+        }
+    }
+
+    private static void atualizarNivel(ArrayList<Jogador> jogadores, Scanner scanner) {
+        System.out.println("Digite o nome do jogador:");
+        String nomeNivel = scanner.next();
+        boolean jogadorNivelEncontrado = false;
+
+        for (Jogador j : jogadores) {
+            if (j.getNome().equalsIgnoreCase(nomeNivel)) {
+                j.setNivel(j.getNivel() + 1);
+                System.out.println("Nível atualizado com sucesso!");
+                jogadorNivelEncontrado = true;
+                break;
+            }
+        }
+        if (!jogadorNivelEncontrado) {
+            System.out.println("Jogador não encontrado.");
+        }
+    }
+
+    private static void consultarJogador(ArrayList<Jogador> jogadores, Scanner scanner) {
+        System.out.println("Digite o nome do jogador que deseja consultar:");
+        String nomeConsulta = scanner.next();
+        boolean encontrado = false;
+
+        for (Jogador j : jogadores) {
+            if (j.getNome().equalsIgnoreCase(nomeConsulta)) {
+                j.exibirInfo();
+                encontrado = true;
+                break;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("Jogador não encontrado.");
+        }
+    }
 
 }
