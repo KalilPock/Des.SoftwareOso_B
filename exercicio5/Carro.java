320a57a68b2c03b26d3322b51f9954ffddf96e17
kalilpock <kalilpock54@gmail.com>
2025-04-08 11:44:53 -0300

    atividade5Finalizada

--- exercicio5/Carro.java
+++ exercicio5/Carro.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Carro {
 
@@ -47,9 +48,104 @@ public class Carro {
         this.placa = placa;
     }
 
+    public static void criarCarro(ArrayList<Carro> carros, Scanner scanner) {
+        System.out.println("Digite o ano do carro:");
+        int ano = scanner.nextInt();
+        System.out.println("Digite a marca do carro:");
+        String marca = scanner.next();
+        System.out.println("Digite o modelo do carro:");
+        String modelo = scanner.next();
+        System.out.println("Digite a velocidade atual do carro:");
+        double velocidadeAtual = scanner.nextDouble();
+        System.out.println("Digite a placa do carro:");
+        String placa = scanner.next();
+
+        Carro carro = new Carro(ano, marca, modelo, velocidadeAtual, placa);
+        carros.add(carro);
+    }
+
+    public static void acelerarCarro(ArrayList<Carro> carros, Scanner scanner) {
+        System.out.println("Digite a placa do carro que deseja acelerar:");
+        String placa = scanner.next();
+        for (Carro carro : carros) {
+            if (carro.getPlaca().equals(placa)) {
+                System.out.println("Digite a quantidade de aceleração:");
+                double aceleracao = scanner.nextDouble();
+                carro.setVelocidadeAtual(carro.getVelocidadeAtual() + aceleracao);
+                System.out.println("Carro acelerado com sucesso!");
+                return;
+            }
+        }
+        System.out.println("Carro não encontrado.");
+    }
+
+    public static void frearCarro(ArrayList<Carro> carros, Scanner scanner) {
+        System.out.println("Digite a placa do carro que deseja frear:");
+        String placa = scanner.next();
+        for (Carro carro : carros) {
+            if (carro.getPlaca().equals(placa)) {
+                System.out.println("Digite a quantidade de desaceleração:");
+                double desaceleracao = scanner.nextDouble();
+                carro.setVelocidadeAtual(carro.getVelocidadeAtual() - desaceleracao);
+                System.out.println("Carro freado com sucesso!");
+                return;
+            }
+        }
+        System.out.println("Carro não encontrado.");
+    }
+
+    public static void consultaCarro(ArrayList<Carro> carros, Scanner scanner) {
+        System.out.println("Digite a placa do carro que deseja consultar:");
+        String placa = scanner.next();
+        for (Carro carro : carros) {
+            if (carro.getPlaca().equals(placa)) {
+                System.out.println("Carro encontrado:");
+                System.out.println("Ano: " + carro.getAno());
+                System.out.println("Marca: " + carro.getMarca());
+                System.out.println("Modelo: " + carro.getModelo());
+                System.out.println("Velocidade Atual: " + carro.getVelocidadeAtual());
+                return;
+            }
+        }
+        System.out.println("Carro não encontrado.");
+    }
 
     public static void exibirMenu(ArrayList<Carro> carros) {
-        System.out.println("Bem-Vindo ao sistema de carros!");
+        Scanner scanner = new Scanner(System.in);
+        boolean sair = true;
+
+        do {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Criar Carro");
+            System.out.println("2 - Acelerar Carro");
+            System.out.println("3 - Frear Carro");
+            System.out.println("4 - Consultar Carro");
+            System.out.println("5 - Sair");
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    criarCarro(carros, scanner);
+                    break;
+                case 2:
+                    acelerarCarro(carros, scanner);
+                    break;
+                case 3:
+                    frearCarro(carros, scanner);
+                    break;
+                case 4:
+                    consultaCarro(carros, scanner);
+                    break;
+                case 5:
+                    sair = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while (sair);
+
+        System.out.println("Sistema encerrado.");
+        scanner.close();
     }
     
 }
